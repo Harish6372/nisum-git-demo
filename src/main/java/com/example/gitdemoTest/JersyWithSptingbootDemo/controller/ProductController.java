479fb649a380bb960e7a7720b0ee4024e7fc6233
sureharish <sureharish63@gmail.com>
2017-11-27 14:57:54 +0530

    second commit

--- src/main/java/com/example/gitdemoTest/JersyWithSptingbootDemo/controller/ProductController.java
+++ src/main/java/com/example/gitdemoTest/JersyWithSptingbootDemo/controller/ProductController.java
@@ -1,5 +1,16 @@
 package com.example.gitdemoTest.JersyWithSptingbootDemo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+public String getProduct()
+	{
+		return "product";
+	}
+	public String getAllProduct()
+	{
+		return "all products";
+	}
 
 }
